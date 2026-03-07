03ef2a7a4af4913f28bad1edfddeb78167be0e66
wilfordluna600-lab <wlunaescobar@byupathway.edu>
2026-03-07 16:40:31 -0500

    changes in Exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name?");
+        String first = Console.ReadLine();
+        Console.Write("What is your last name?");
+        String last = Console.ReadLine();
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
